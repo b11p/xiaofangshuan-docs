028e290a00ef5bae58311280530812b02bf4843b
bleatingsheep <bleatingsheep@live.cn>
2021-05-12 19:04:52 +0900

    Update config.

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,6 +9,18 @@ export default defineUserConfig<DefaultThemeOptions>({
     themeConfig: {
         navbar: [
             { text: '主页', link: '/' },
+            {
+                text: '功能列表', children: [{
+                    text: 'osu! 相关功能',
+                    link: 'https://github.com/b11p/OsuQqBotForNewbieGroup/wiki/osu!-%E7%9B%B8%E5%85%B3%E5%8A%9F%E8%83%BD'
+                }, {
+                    text: '图片类功能',
+                    link: 'https://github.com/b11p/OsuQqBotForNewbieGroup/wiki/%E5%9B%BE%E7%89%87%E5%8A%9F%E8%83%BD'
+                    }, {
+                    text: '实用功能',
+                    link:'https://github.com/b11p/OsuQqBotForNewbieGroup/wiki/%E5%AE%9E%E7%94%A8%E5%8A%9F%E8%83%BD'
+                }]
+            },
             { text: '分身', link: '/fenshen/' },
         ],
         repo: 'b11p/OsuQqBotForNewbieGroup',
